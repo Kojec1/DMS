9de044e2d51ec1d3d3534f7dfb86640b4ccda048
Kojec1 <konrad.pawlik333@gmail.com>
2025-07-20 15:26:07 +0200

    Fix augmentation in WFLW and 300W dataset classes

--- data/dataset.py
+++ data/dataset.py
@@ -402,7 +402,7 @@ class WFLWDataset(BaseDataset):
 
         # Horizontal Flip
         if self.is_train and self.flip_aug and random.random() > 0.5:
-            image, landmarks, _, _ = horizontal_flip(image, landmarks, effective_width)
+            image, landmarks, _, _ = horizontal_flip(image, landmarks, effective_width=effective_width)
             # WFLW landmark flipping indices (symmetric pairs)
             flip_indices = [32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16,
                            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
@@ -666,7 +666,7 @@ class Face300WDataset(BaseDataset):
 
         # Horizontal Flip
         if self.is_train and self.flip_aug and random.random() > 0.5:
-            image, landmarks, _, _ = horizontal_flip(image, landmarks, effective_width)
+            image, landmarks, _, _ = horizontal_flip(image, landmarks, effective_width=effective_width)
             # 68-point landmark flipping indices (iBUG 68-point standard)
             flip_indices = [16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
                            26, 25, 24, 23, 22, 21, 20, 19, 18, 17,
